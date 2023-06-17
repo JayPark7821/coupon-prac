48b304e59e542e1ebc05868c9c6cf9142907479b
jaypark <pjhyun1990@gmail.com>
2023-06-18 00:03:05 +0900

    kafka producer

--- api/src/main/java/kr/jay/api/service/ApplyService.java
+++ api/src/main/java/kr/jay/api/service/ApplyService.java
@@ -3,6 +3,7 @@ package kr.jay.api.service;
 import org.springframework.stereotype.Service;
 
 import kr.jay.api.domain.Coupon;
+import kr.jay.api.producer.CouponCreateProducer;
 import kr.jay.api.repository.CouponCountRepository;
 import kr.jay.api.repository.CouponRepository;
 
@@ -11,10 +12,16 @@ public class ApplyService {
 
 	private final CouponRepository couponRepository;
 	private final CouponCountRepository couponCountRepository;
+	private final CouponCreateProducer couponCreateProducer;
 
-	public ApplyService(final CouponRepository couponRepository, final CouponCountRepository couponCountRepository) {
+	public ApplyService(
+		final CouponRepository couponRepository,
+		final CouponCountRepository couponCountRepository,
+		final CouponCreateProducer couponCreateProducer
+	) {
 		this.couponRepository = couponRepository;
 		this.couponCountRepository = couponCountRepository;
+		this.couponCreateProducer = couponCreateProducer;
 	}
 
 	public void apply(final Long userId) {
@@ -23,6 +30,6 @@ public class ApplyService {
 		if (count > 100) {
 			return;
 		}
-		couponRepository.save(new Coupon(userId));
+		couponCreateProducer.create(userId);
 	}
 }

2db2613491edda2623bebc4133eafebcaf3fb857
jaypark <pjhyun1990@gmail.com>
2023-06-17 22:45:52 +0900

    redis로 해결 시도

---
+++ api/src/main/java/kr/jay/api/repository/CouponCountRepository.java
@@ -0,0 +1,17 @@
+package kr.jay.api.repository;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CouponCountRepository {
+	private final RedisTemplate<String, String> redisTemplate;
+
+	public CouponCountRepository(final RedisTemplate<String, String> redisTemplate) {
+		this.redisTemplate = redisTemplate;
+	}
+
+	public Long increment() {
+		return redisTemplate.opsForValue().increment("couponCount");
+	}
+}
--- api/src/main/java/kr/jay/api/service/ApplyService.java
+++ api/src/main/java/kr/jay/api/service/ApplyService.java
@@ -3,19 +3,23 @@ package kr.jay.api.service;
 import org.springframework.stereotype.Service;
 
 import kr.jay.api.domain.Coupon;
+import kr.jay.api.repository.CouponCountRepository;
 import kr.jay.api.repository.CouponRepository;
 
 @Service
 public class ApplyService {
 
 	private final CouponRepository couponRepository;
+	private final CouponCountRepository couponCountRepository;
 
-	public ApplyService(final CouponRepository couponRepository) {
+	public ApplyService(final CouponRepository couponRepository, final CouponCountRepository couponCountRepository) {
 		this.couponRepository = couponRepository;
+		this.couponCountRepository = couponCountRepository;
 	}
 
 	public void apply(final Long userId) {
-		final long count = couponRepository.count();
+		final Long count = couponCountRepository.increment();
+
 		if (count > 100) {
 			return;
 		}
